d09acb33aaf499f79405d071484bfdbc60ecc2d3
Dolgih-Nastya <dolgih.nastya@gmail.com>
2013-09-14 19:48:55 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyGarageTestApplication::Application.config.secret_token = 'fca4dc2f17ef7547a82c742864af48de5bdd89b9577b136a1250a00544caa6d047064925c36b76584beb0a68d60af8bcd815c69c5bde617d2be1b012cf67713a'
